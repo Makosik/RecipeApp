74ee1e214f582aab7f024fc750dfcb9984c93360
Maks <an-tar70@ya.ru>
2024-03-21 19:57:01 +0700

    added new component and some changes in controllers

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -1,9 +1,14 @@
-import React, { useEffect, useState } from 'react'
-import DataComponent from './components/dataComponent';
-   function App() {
-      return (
-         <DataComponent></DataComponent>
-      )
-   }
+import React from 'react'
+import DataComponent from './components/DataComponent';
+import AddDishForm from './components/AddDishesTitle';
+
+function App() {
+   return (
+      <>
+         <AddDishForm />
+         <DataComponent />
+      </>
+   )
+}
 
 export default App
--- src/server/controllers/dishes.controller.js
+++ src/server/controllers/dishes.controller.js
@@ -2,13 +2,18 @@ const db = require("../db");
 
 class DishesController {
    async createDish(req, res) {
-      const [title] = req.body;
-      //const newDish = await db.query("insert into Dishes (title) values (яичница) returning *")
-      console.log(title);
-      res.json(newDish.row);
+      const { title } = req.body;
+      try {
+         const newDish = await db.query("INSERT INTO Dishes (title) VALUES ($1) RETURNING *", [title]);
+         res.json(newDish.rows[0]);
+      } catch (error) {
+         console.error("Error inserting dish:", error);
+         res.status(500).json({ error: "Error inserting dish" });
+      }
    }
+
    async getDishes(req, res) {
-      const dishes = await db.query("select * from dishes")
+      const dishes = await db.query("select * from dishes;")
       res.json(dishes.rows);
    }
 }
