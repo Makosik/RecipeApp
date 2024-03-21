0b257e2055126a7662ca4d0ca7e8d7f06b93ca48
Maks <an-tar70@ya.ru>
2024-03-21 14:58:48 +0700

    created controllers folder with first component

---
+++ src/client/src/components/dataComponent.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const DataComponent = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get('http://localhost:5000/api/dishes');
+      setData(result);
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h1>Data from PostgreSQL</h1>
+      <ul>
+        {data.map(item => (
+          <li key={item.id}>{item.title}</li> // Replace 'columnName' with actual column name
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default DataComponent;
