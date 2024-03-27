44136d7fee45627d5ada6a5c33944e755d85ea8a
Maks <an-tar70@ya.ru>
2024-03-27 22:32:08 +0700

    created new files for redux toolkit

---
+++ client/src/ordersSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const ordersSlice = createSlice({
+  name: 'orders',
+  initialState: {
+    orders: [],
+  },
+  reducers: {
+    setOrders: (state, action) => {
+      state.orders = action.payload;
+    },
+  },
+});
+
+export const { setOrders } = ordersSlice.actions;
+
+export const selectOrders = (state) => state.orders.orders;
+
+export default ordersSlice.reducer;
